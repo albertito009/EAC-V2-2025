d33804510e099f962f50651ebd77081ff1a052eb
isard <isard@debian>
2025-01-27 19:17:41 +0100

    parte 1 hecha

--- eacv2/eacv2/settings.py
+++ eacv2/eacv2/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-f%z!idy!0t75(6%*e(ujy(xh4@#(io7qr5q#$e)xp0q*@lq)-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
