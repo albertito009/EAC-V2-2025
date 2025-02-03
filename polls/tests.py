082a5ca667ca1916ca097c3c4df602308ae0cc53
isard <isard@debian>
2025-02-03 19:23:43 +0100

    Test OK

--- polls/tests.py
+++ polls/tests.py
@@ -1,27 +1,32 @@
 from django.test import TestCase
-
-# Create your tests here.
-
-from django.test import TestCase
 from django.contrib.auth.models import User
+from selenium import webdriver  # Importar Selenium WebDriver
+from selenium.webdriver.chrome.options import Options  # Importar Options para configurar el navegador
 
 class PollsTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+
+        # Configurar las opciones del navegador (en este caso, Chrome)
         opts = Options()
-        cls.selenium = WebDriver(options=opts)
-        cls.selenium.implicitly_wait(5)
-        # creem superusuari
+        opts.add_argument("--headless")  # Ejecutar en modo sin cabeza (sin interfaz gráfica)
+        opts.add_argument("--disable-gpu")  # Deshabilitar GPU para evitar errores
+        opts.add_argument("--no-sandbox")  # Necesario en algunos entornos
+
+        # Inicializar el WebDriver de Selenium con las opciones configuradas
+        cls.selenium = webdriver.Chrome(options=opts)
+        cls.selenium.implicitly_wait(5)  # Esperar hasta 5 segundos para encontrar elementos
+
+        # Crear un superusuario para las pruebas
         user = User.objects.create_user("isard", "isard@isardvdi.com", "pirineus")
         user.is_superuser = True
         user.is_staff = True
         user.save()
 
-
+    @classmethod
     def tearDownClass(cls):
-        # tanquem browser
-        # comentar la propera línia si volem veure el resultat de l'execució al navegador
+        # Cerrar el navegador al finalizar las pruebas
         cls.selenium.quit()
         super().tearDownClass()
 
